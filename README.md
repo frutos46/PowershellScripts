68be6c0a7763f5a870abf2710fc94515ec87ff04
Jose Antonio <jose.antonio.frutos.puerto@ing.com>
2024-03-29 18:47:05 +0100

    29032024

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PowershellScripts
+# Utilidades
+
+Iré dejando script y herramientas útiles para facilitar el trabajo de administración de nuestros sistemas
---
+++ Remove-Flash-IE.ps1
@@ -0,0 +1,18 @@
+﻿####################################################################################
+# Autor: Jose Antonio Frutos
+# Version: v1
+# Creado: 08/03/2022
+# Script: Desinstala tanto el flash como IE11
+####################################################################################
+
+
+New-Item C:\tmp -ItemType Directory -Force
+
+Invoke-WebRequest -Uri 'https://fpdownload.macromedia.com/get/flashplayer/current/support/uninstall_flash_player.exe' -OutFile 'c:\tmp\uninstall_flash_player.exe'
+
+c:\tmp\uninstall_flash_player.exe -uninstall
+
+
+
+Remove-Item -Path "c:\tmp\uninstall_flash_player.exe" 
+Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 –Online -NoRestart
